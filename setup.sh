c8557abef974d1f593474d1ccec354d820ee6d8f
felix043 <tranfelix99@gmail.com>
2023-02-28 20:14:23 +0100

    change waybar to be a little bit more cool

--- setup.sh
+++ setup.sh
@@ -11,10 +11,3 @@ unlink $HOME/.config/setup.sh
 unlink $HOME/.config/dotFiles
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
-if [ "$1" = "laptop" ]; then
-  ln -s $PWD/eww_laptop $HOME/.config/eww
-  ln -s $PWD/hypr_laptop $HOME/.config/hypr
-elif [ "$1" = "desktop" ]; then 
-  ln -s $PWD/eww_desktop $HOME/.config/eww
-  ln -s $PWD/hypr_desktop $HOME/.config/hypr
-fi
